5a44860056262d23c2a98185a8a2d87f7a3e472e
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-12-25 17:49:46 +0100

    Fixed template and create new VariantCustomizer project

---
+++ VariantCustomizer/VariantCustomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using PluginConfig.API;
+
+namespace VariantCustomizer {
+    [BepInPlugin("com.einfachirgendwa1.variantCustomizer", "VariantCustomizer", "1.0.0.0")]
+    public class VariantCustomizer : BaseUnityPlugin {
+        private readonly Harmony harmony = new Harmony("VariantCustomizer");
+
+        private PluginConfigurator? config;
+
+        private void Awake() {
+            string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+            config = PluginConfigurator.Create("Variant Customizer", "com.einfachirgendwa1.variantCustomizer");
+            config.SetIconWithURL($"file://{Path.Combine(directory, "icon.png")}");
+        }
+    }
+}
