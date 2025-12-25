6ee42e9fc21dda761718a5b282e92e43fc6c774e
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2025-12-25 02:29:41 +0100

    Removed dumb shit

--- UltrakillMod/UltrakillMod.cs
+++ UltrakillMod/UltrakillMod.cs
@@ -1,7 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using BepInEx;
-using BepInEx.Logging;
 using HarmonyLib;
 using UltrakillMod.UnityWav;
 using UnityEngine;
@@ -10,20 +10,14 @@ namespace UltrakillMod {
     [BepInPlugin("UltrakillMod", "UltrakillMod", "1.0.0.0")]
     public class UltrakillMod : BaseUnityPlugin {
         private static AudioClip? audioClip;
-        private static ManualLogSource? staticLogger;
         private readonly Harmony harmony = new Harmony("UltrakillMod");
 
         private void Awake() {
-            staticLogger = Logger;
-            foreach (string resource in Assembly.GetExecutingAssembly().GetManifestResourceNames()) {
-                Logger.LogMessage(resource);
-            }
-
             using Stream stream = Assembly.GetExecutingAssembly()
                 .GetManifestResourceStream("UltrakillMod.EmbeddedResources.ThrowCoinSound.wav")!;
 
             byte[] sound = new byte[stream.Length];
-            stream.Read(sound, 0, sound.Length);
+            if (stream.Read(sound, 0, sound.Length) != sound.Length) throw new Exception();
 
             MethodInfo throwCoin = AccessTools.Method(typeof(Revolver), "ThrowCoin")!;
             HarmonyMethod prefix = new HarmonyMethod(typeof(UltrakillMod), nameof(PlaySound));
@@ -31,15 +25,7 @@ namespace UltrakillMod {
             audioClip = WavUtility.ToAudioClip(sound);
         }
 
-        private void Update() {
-            if (Input.GetKeyDown(KeyCode.B)) {
-                Logger.LogError("B Key was pressed!");
-                AudioSource.PlayClipAtPoint(audioClip!, Vector3.zero);
-            }
-        }
-
         private static void PlaySound(Revolver __instance) {
-            staticLogger!.LogError("Playing Sound");
             FieldInfo gunAud = typeof(Revolver).GetField("gunAud", BindingFlags.NonPublic | BindingFlags.Instance)!;
             AudioSource audio = (AudioSource)gunAud.GetValue(__instance);
             AudioSource newSource = audio.gameObject.AddComponent<AudioSource>();
